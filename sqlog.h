91f71feb10633cd2cf114639d5b7e5d234f88387
Yunkeun Kim <yunkeun0633@gmail.com>
2025-06-23 21:10:36 +0100

    Add unittest case code of sqlog

--- sqlog.h
+++ sqlog.h
@@ -28,8 +28,50 @@ typedef enum
     LOG_LEVEL_D      // DEBUG
 } LOG_LEVEL;
 
-// Declaration of SqLog function
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// Global variables for testing
+extern unsigned char g_key[LOG_AES_KEY_LEN];
+
+// Main logging function
 int SqLog(int log_level, const char *format, ...);
+
+// Initialization functions
+int SqLog_InitWriting(const char *key_file, const char *logf_path, const char *logf_name);
+int SqLog_InitReading(const char *key_file);
+
+// Logging functions
+int WriteLog(const char *message);
+int Sqlog_WriteLog(const char *message);  // Alternative name for WriteLog
+
+// Log reading and decryption functions
+int SqLog_ReadLog(const char *pacCiperText, size_t szCLen, char *pacPlainText, size_t szPLen);
+
+// Log integrity verification
+int SqLog_VerifyIntegrity(const char *log_file_path);
+
+// Log management functions
+void SqLog_LogStart(int argc, char *argv[]);
+void SqLog_CloseFiles(void);
+
+// Utility functions (for testing purposes)
+int hex_to_bin(const char *hex, unsigned char *bin, size_t bin_len);
+int load_key(const char *pcFile, unsigned char *pKeyBuf);
+int base64_encode(const unsigned char *in, size_t in_len, char *out, const unsigned int out_len);
+int base64_decode(const char *in, unsigned char *out, unsigned int *out_len);
+int decrypt_gcm(const unsigned char *key, const unsigned char *data, size_t data_len, 
+                char *plaintext_out, size_t plaintext_max);
+int save_logfile_hmac(void);
+int rotate_log_file_if_needed(size_t new_entry_size);
+int open_new_log_file(void);
+int get_max_log_index(void);
+long get_file_size(const char *filename);
+
+#ifdef __cplusplus
+}
+#endif
 
 // Convenience macros for each log level (support variadic args)
 #define SqLog_F(...) SqLog(LOG_LEVEL_F, __VA_ARGS__)
@@ -38,13 +80,4 @@ int SqLog(int log_level, const char *format, ...);
 #define SqLog_I(...) SqLog(LOG_LEVEL_I, __VA_ARGS__)
 #define SqLog_D(...) SqLog(LOG_LEVEL_D, __VA_ARGS__)
 
-int SqLog_InitWriting(const char *key_file, const char *logf_path, const char *logf_name);
-void SqLog_LogStart(int argc, char *argv[]);
-int Sqlog_WriteLog(const char *message);
-void SqLog_CloseFiles(void);
-int SqLog_VerifyIntegrity(const char *log_file_path);
-int WriteLog(const char *message);
-
-int SqLog_InitReading(const char *key_file);
-int SqLog_ReadLog(const char *, size_t, char *, size_t);
 #endif // __SQLOG_H__
